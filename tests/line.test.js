15d60a2035d402cd6bbb5566ed454441b7de5420
Scott Kao <scottxxx666@gmail.com>
2019-07-05 02:54:26 +0800

    Refactor and clearAllMocks before each

--- tests/line.test.js
+++ tests/line.test.js
@@ -3,13 +3,21 @@ const got = require('got');
 
 jest.mock('got');
 
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
 async function retrieveToken() {
-    got.mockResolvedValue({body: JSON.stringify({access_token: 'token'})});
+    got.mockResolvedValue({body: JSON.stringify({access_token: 'fake token'})});
     const token = await line.retrieveToken('code', 'redirectUri', 'clientId', 'clientSecret', 'url');
     return token;
 }
 
-test('Call api and retrieve token', async () => {
+test('Retrieve token should get a token', async () => {
+    expect(await retrieveToken()).toBe('fake token');
+});
+
+test('Retrieve token should call api once', async () => {
     await retrieveToken();
     expect(got.mock.calls.length).toBe(1);
 });
